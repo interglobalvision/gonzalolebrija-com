f8223229fba9954942db269eb6d1bd67880c36d9
Patrick Best <patrickbest@patrickbest.com>
2016-03-09 18:24:05 -0600

    add metaboxes for post types

--- lib/meta-boxes.php
+++ lib/meta-boxes.php
@@ -31,13 +31,123 @@ return $post_options;
 add_action( 'cmb2_init', 'igv_cmb_metaboxes' );
 function igv_cmb_metaboxes() {
 
-	// Start with an underscore to hide fields from custom fields list
-	$prefix = '_igv_';
+  // Start with an underscore to hide fields from custom fields list
+  $prefix = '_igv_';
 
-	/**
-	 * Metaboxes declarations here
+  /**
+   * Metaboxes declarations here
    * Reference: https://github.com/WebDevStudios/CMB2/blob/master/example-functions.php
-	 */
+   */
+
+   // exhibitions
+
+  $exposiciones_meta = new_cmb2_box( array(
+    'id'            => $prefix . 'exposiciones_metabox',
+    'title'         => __( 'Meta', 'cmb2' ),
+    'object_types'  => array( 'exposiciones', ), // Post type
+  ) );
+
+  $exposiciones_meta->add_field( array(
+    'name'       => __( 'Start date', 'cmb2' ),
+    'desc'       => __( 'start of the exhibition', 'cmb2' ),
+    'id'         => $prefix . 'start_date',
+    'type'       => 'text_date',
+  ) );
+
+  $exposiciones_meta->add_field( array(
+    'name'       => __( 'End date', 'cmb2' ),
+    'desc'       => __( 'end of the exhibition', 'cmb2' ),
+    'id'         => $prefix . 'end_date',
+    'type'       => 'text_date',
+  ) );
+
+  $exposiciones_meta->add_field( array(
+    'name'       => __( 'Works gallery', 'cmb2' ),
+    'desc'       => __( 'as a wordpress gallery', 'cmb2' ),
+    'id'         => $prefix . 'works_gallery',
+    'type'       => 'wysiwyg',
+  ) );
+
+  $exposiciones_meta->add_field( array(
+    'name'       => __( 'Installation gallery', 'cmb2' ),
+    'desc'       => __( 'as a wordpress gallery', 'cmb2' ),
+    'id'         => $prefix . 'installation_gallery',
+    'type'       => 'wysiwyg',
+  ) );
+
+  // works
+
+  $obra_meta = new_cmb2_box( array(
+    'id'            => $prefix . 'obra_metabox',
+    'title'         => __( 'Meta', 'cmb2' ),
+    'object_types'  => array( 'obra', ), // Post type
+  ) );
+
+  $obra_meta->add_field( array(
+    'name'       => __( 'Year', 'cmb2' ),
+    'desc'       => __( '', 'cmb2' ),
+    'id'         => $prefix . 'year',
+    'type'       => 'text',
+  ) );
+
+  $obra_meta->add_field( array(
+    'name'       => __( 'Medium', 'cmb2' ),
+    'desc'       => __( '', 'cmb2' ),
+    'id'         => $prefix . 'medium',
+    'type'       => 'text',
+  ) );
+
+  $obra_meta->add_field( array(
+    'name'       => __( 'Size', 'cmb2' ),
+    'desc'       => __( '', 'cmb2' ),
+    'id'         => $prefix . 'size',
+    'type'       => 'text',
+  ) );
+
+  $obra_meta->add_field( array(
+    'name'       => __( 'Gallery', 'cmb2' ),
+    'desc'       => __( 'as a wordpress gallery', 'cmb2' ),
+    'id'         => $prefix . 'gallery',
+    'type'       => 'wysiwyg',
+  ) );
+
+  // journal
+
+  $journal_meta = new_cmb2_box( array(
+    'id'            => $prefix . 'journal_metabox',
+    'title'         => __( 'Meta', 'cmb2' ),
+    'object_types'  => array( 'journal', ), // Post type
+  ) );
+
+  $journal_meta->add_field( array(
+    'name'       => __( 'Large image', 'cmb2' ),
+    'desc'       => __( 'scan of feature or similar', 'cmb2' ),
+    'id'         => $prefix . 'image',
+    'type'       => 'file',
+  ) );
+
+  // publications
+
+  $publicaciones_meta = new_cmb2_box( array(
+    'id'            => $prefix . 'publicaciones_metabox',
+    'title'         => __( 'Meta', 'cmb2' ),
+    'object_types'  => array( 'publicaciones', ), // Post type
+  ) );
+
+  $publicaciones_meta->add_field( array(
+    'name'       => __( 'Year', 'cmb2' ),
+    'desc'       => __( '', 'cmb2' ),
+    'id'         => $prefix . 'year',
+    'type'       => 'text',
+  ) );
+
+  $publicaciones_meta->add_field( array(
+    'name'       => __( 'Gallery', 'cmb2' ),
+    'desc'       => __( 'as a wordpress gallery', 'cmb2' ),
+    'id'         => $prefix . 'gallery',
+    'type'       => 'wysiwyg',
+  ) );
+
 
 }
 ?>
