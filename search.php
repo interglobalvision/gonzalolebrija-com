bce72680d84b603da60de3ad624a4101e4cd3989
Patrick Best <patrickbest@patrickbest.com>
2016-03-15 13:50:35 -0600

    scaffold search results

---
+++ search.php
@@ -0,0 +1,70 @@
+<?php
+get_header();
+?>
+
+<!-- main content -->
+
+<main id="main-content" class="container">
+
+  <div class="row">
+
+    <div class="col col-6">
+
+      <?php
+        if (have_posts()) {
+          echo $wp_query->post_count . ' ' . __('[:es]Resultados[:en]Results');
+        }
+      ?>
+
+    </div>
+
+    <!-- main posts loop -->
+    <section id="posts" class="col col-18">
+
+<?php
+if( have_posts() ) {
+  while( have_posts() ) {
+    the_post();
+?>
+
+      <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+        <header class="post-header">
+
+          <a href="<?php the_permalink() ?>"><h5 class="post-type">
+            <?php
+              if (get_post_type() !== 'post') {
+                echo get_post_type();
+              } else {
+                echo __('[:es]Noticias[:en]News');
+              }
+            ?>
+          </h5></a>
+
+          <a href="<?php the_permalink() ?>"><h2 class="post-title"><?php the_title(); ?></h2></a>
+
+        </header>
+
+      </article>
+
+<?php
+  }
+} else {
+?>
+    <article class="u-alert"><?php _e('Sorry, no posts matched your criteria'); ?></article>
+<?php
+} ?>
+
+    <?php get_template_part('partials/pagination'); ?>
+
+    <!-- end posts -->
+    </section>
+
+
+<!-- end main-content -->
+
+</main>
+
+<?php
+get_footer();
+?>
