fdc2c433d1459f41df450af822f1fa39a7f18bdd
Patrick Best <patrickbest@patrickbest.com>
2016-03-16 18:24:09 -0600

    add second row of home

--- front-page.php
+++ front-page.php
@@ -81,7 +81,7 @@ $date_format = 'd M, y';
         <?php
           $args = array(
             'post_type' => 'post',
-            'posts_per_page' => 2,
+            'posts_per_page' => 1,
           );
           $news = new WP_Query($args);
           if ($news->have_posts()) {
@@ -129,6 +129,104 @@ $date_format = 'd M, y';
             <div class="home-column-post-copy">
               <?php the_excerpt();?>
             </div>
+          </div>
+        <?php
+          }
+          wp_reset_postdata();
+        ?>
+      </div>
+    </div>
+  </div>
+
+<div class="row">
+    <div class="col col-6">
+      <div class="home-column">
+        <h4 class="underline"><?php echo __('[:es]Publicaciones[:en]Publications'); ?></h4>
+        <?php
+          $args = array(
+            'post_type' => 'publicaciones',
+            'posts_per_page' => 2,
+          );
+          $publications = new WP_Query($args);
+          if ($publications->have_posts()) {
+            $publications->the_post();
+            $meta = get_post_meta($post->ID);
+        ?>
+          <div class="home-column-post">
+            <h3><?php the_title(); ?></h3>
+            <?php the_post_thumbnail(); ?>
+            <div class="home-column-post-copy">
+              <?php the_excerpt();?>
+            </div>
+          </div>
+        <?php
+          }
+          wp_reset_postdata();
+        ?>
+      </div>
+    </div>
+    <div class="col col-12">
+        <?php
+          $args = array(
+            'post_type' => 'post',
+            'posts_per_page' => 1,
+            'offset' => 1
+          );
+          $news = new WP_Query($args);
+          if ($news->have_posts()) {
+            $news->the_post();
+        ?>
+          <div class="home-news-post">
+            <header class="home-news-post-header">
+              <h4><?php the_time($date_format) ?></h4>
+              <h2><?php the_title(); ?></h2>
+            </header>
+            <?php the_post_thumbnail(); ?>
+            <div class="home-news-post-copy">
+              <?php the_excerpt();?>
+            </div>
+          </div>
+        <?php
+          }
+          wp_reset_postdata();
+        ?>
+    </div>
+    <div class="col col-6">
+      <div class="home-column">
+        <h4 class="underline"><?php echo __('[:es]Obra[:en]Works'); ?></h4>
+        <?php
+          $args = array(
+            'post_type' => 'obra',
+            'posts_per_page' => 1,
+          );
+          $works = new WP_Query($args);
+          if ($works->have_posts()) {
+            $works->the_post();
+            $meta = get_post_meta($post->ID);
+        ?>
+          <div class="home-column-post">
+            <?php the_post_thumbnail(); ?>
+            <h4 class="u-align-center"><?php the_title(); ?> <?php if (!empty($meta['_igv_year'][0])) {echo $meta['_igv_year'][0]; } ?></h4>
+          </div>
+        <?php
+          }
+          wp_reset_postdata();
+        ?>
+        <h4 class="underline"><?php echo __('[:es]En exhibición[:en]Works on view'); ?></h4>
+        <?php
+          $args = array(
+            'post_type' => 'obra',
+            'posts_per_page' => 1,
+            // category or wtf chooses works on view
+          );
+          $works = new WP_Query($args);
+          if ($works->have_posts()) {
+            $works->the_post();
+            $meta = get_post_meta($post->ID);
+        ?>
+          <div class="home-column-post">
+            <?php the_post_thumbnail(); ?>
+            <h4 class="u-align-center"><?php the_title(); ?> <?php if (!empty($meta['_igv_year'][0])) {echo $meta['_igv_year'][0]; } ?></h4>
           </div>
         <?php
           }
