31ef1c92f3b538ac1a7e02e7e0bf1bb3b18c7af8
cas <casska@gmail.com>
2016-03-22 14:21:49 -0600

    add global Debounce function

--- js/library.js
+++ js/library.js
@@ -1,3 +1,18 @@
+Debounce = function(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
 /**
  * Swiper 3.3.1
  * Most modern mobile touch slider and framework with hardware accelerated transitions
