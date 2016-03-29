c7baf5d7a1c40cb2a561b59f5fd1c485e51e5979
cas <casska@gmail.com>
2016-03-28 23:01:35 -0600

    rever fuckup in footer scripts

--- partials/scripts.php
+++ partials/scripts.php
@@ -1,7 +1,5 @@
 <section id="scripts">
-<?php if (!WP_DEBUG) { ?>
   <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-<?php } ?>
   <script>window.jQuery || document.write('<script src="<?php bloginfo('stylesheet_directory'); ?>/js/vendor/jquery-2.1.1.min.js"><\/script>')</script>
   <?php wp_footer(); ?>
 </section>
