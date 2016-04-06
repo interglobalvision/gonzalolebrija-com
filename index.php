b2b95a0afaab69c4076ea33630276e409f9ea45c
Patrick Best <patrickbest@patrickbest.com>
2016-04-05 19:12:58 -0500

    set archive.php for index.php too

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
 get_header();
+
+if (qtranxf_getLanguage() == 'es') {
+  $locale = 'es_ES';
+} else {
+  $locale = 'en_US';
+}
+
+\Moment\Moment::setLocale($locale);
+
+// current time
+$now = new \Moment\Moment();
+
+$date_format = 'M., y';
+
+$monthNumber = get_query_var('monthnum');
 ?>
 
 <!-- main content -->
@@ -9,6 +24,30 @@ get_header();
   <div class="row">
 
     <div class="col col-6">
+      <ul id="month-filter" class="filter-list">
+        <?php
+
+          $months = get_all_months(array('post'), 'DESC');
+
+          foreach($months as $month) {
+
+            $monthMoment = new \Moment\Moment($month->month . '/1/' . $month->year);
+            $link = get_month_link($monthMoment->format('Y'), $monthMoment->format('n'));
+
+            echo '<li>';
+            if ($monthNumber == $monthMoment->format('n')) {
+              echo '<a class="filter-term active" href="' . $link . '">' . $monthMoment->format($date_format) . '</a>';
+            } else {
+              echo '<a class="filter-term" href="' . $link . '">' . $monthMoment->format($date_format) . '</a>';
+            }
+            echo '</li>';
+          }
+        ?>
+
+        <li>&nbsp;</li>
+        <li><a href="<?php echo home_url('noticias/'); ?>" class="filter-term filter-term-all <?php echo $year_param === 'all' ? 'active' : ''; ?>"><?php echo __('[:es]Todos[:en]All'); ?></a></li>
+
+      </ul>
 
     </div>
 
@@ -21,19 +60,15 @@ if( have_posts() ) {
     the_post();
 ?>
 
-      <article <?php post_class('col col-18'); ?> id="post-<?php the_ID(); ?>">
+      <article <?php post_class('col col-18 archive-noticia-post u-align-center'); ?> id="post-<?php the_ID(); ?>">
 
         <header class="post-header">
 
           <a href="<?php the_permalink() ?>"><h4 class="post-date"><?php the_time('M., Y'); ?></h4></a>
 
-          <a href="<?php the_permalink() ?>"><h2 class="post-title"><?php the_title(); ?></h2></a>
+          <a href="<?php the_permalink() ?>"><h2 class="post-title font-spaced js-fix-widows"><?php the_title(); ?></h2></a>
 
         </header>
-
-        <div class="copy">
-          <?php the_content(); ?>
-        </div>
 
       </article>
 
