0e080ab9312d323eea5efd6e74955fe4a50ed87f
Patrick Best <patrickbest@patrickbest.com>
2016-04-08 18:19:44 -0500

    use Swiper custom pagination to render paging

--- partials/gallery-nav.php
+++ partials/gallery-nav.php
@@ -1,5 +1,5 @@
 <?php
 ?>
 <nav id="single-work-gallery-nav">
-  <span class="js-gallery-prev u-pointer">< </span><span id="gallery-index-active">0</span> / <span id="gallery-index-length">0</span><span class="js-gallery-next u-pointer"> ></span>
+  <span class="js-gallery-prev u-pointer">< </span><span class="js-gallery-next u-pointer"> ></span>
 </nav>
