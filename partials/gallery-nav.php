f99ba369ebb47e30322224e4a029a7a4e0a55612
Patrick Best <patrickbest@patrickbest.com>
2016-04-15 14:14:20 -0500

    move nav arrows outside Swiper custom pagination
     I guess it dont rebind otherwise

--- partials/gallery-nav.php
+++ partials/gallery-nav.php
@@ -1,5 +1,5 @@
 <?php
 ?>
 <nav id="single-work-gallery-nav">
-  <span class="js-gallery-prev u-pointer">< </span><span class="js-gallery-next u-pointer"> ></span>
+  <span class="js-gallery-prev u-pointer">< </span><span id="single-work-gallery-pagination"></span><span class="js-gallery-next u-pointer"> ></span>
 </nav>
