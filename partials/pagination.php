6993e38fb8ffe23ef7de7503072cdbacde43a85d
Patrick Best <patrickbest@patrickbest.com>
2016-05-03 20:24:45 -0500

    fix pagination

--- partials/pagination.php
+++ partials/pagination.php
@@ -1,21 +1,23 @@
 <?php
-if( get_next_posts_link() || get_previous_posts_link() ) {
+if (get_next_posts_link() || get_previous_posts_link()) {
 ?>
   <!-- post pagination -->
-  <nav id="pagination">
+  <nav id="pagination" class="u-float">
+    <div class="col col-18">
 <?php
-$previous = get_previous_posts_link('Newer');
-$next = get_next_posts_link('Older');
+$previous = get_previous_posts_link(__('[:es]más nuevo[:en]newer'));
+$next = get_next_posts_link(__('[:es]más viejo[:en]older'));
 if ($previous) {
   echo $previous;
 }
 if ($previous && $next) {
-  echo ' &mdash; ';
+  echo ' / ';
 }
 if ($next) {
   echo $next;
 }
 ?>
+    </div>
   </nav>
 <?php
 }
