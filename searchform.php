5174220146571182573ff58853b073b61f7a8153
cas <casska@gmail.com>
2016-05-09 15:04:11 -0500

    fix #76 add search input label

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-  <?php echo __('[:es]Buscar:[:en]Search:'); ?><input type="search" class="search-field" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+  <label><?php echo __('[:es]Buscar:[:en]Search:'); ?><input type="search" class="search-field" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" /></label>
   <button type="submit" class="search-submit u-hidden">search</button>
 </form>
