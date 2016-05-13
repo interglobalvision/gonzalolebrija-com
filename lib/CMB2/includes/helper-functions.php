aee4d2854f7976ffa6187b8525a2444a595704d7
cas <casska@gmail.com>
2016-05-13 15:39:56 -0500

    update CMB2

--- lib/CMB2/includes/helper-functions.php
+++ lib/CMB2/includes/helper-functions.php
@@ -29,7 +29,13 @@ function cmb2_autoload_classes( $class_name ) {
 		return;
 	}
 
-	include_once( cmb2_dir( "includes/{$class_name}.php" ) );
+	$path = 'includes';
+
+	if ( 'CMB2_Type' === $class_name || 0 === strpos( $class_name, 'CMB2_Type_' ) ) {
+		$path .= '/types';
+	}
+
+	include_once( cmb2_dir( "$path/{$class_name}.php" ) );
 }
 
 /**
@@ -49,9 +55,7 @@ function cmb2_utils() {
  * @return CMB2_Ajax object CMB2 utilities class
  */
 function cmb2_ajax() {
-	static $cmb2_ajax;
-	$cmb2_ajax = $cmb2_ajax ? $cmb2_ajax : new CMB2_Ajax();
-	return $cmb2_ajax;
+	return CMB2_Ajax::get_instance();
 }
 
 /**
@@ -304,5 +308,49 @@ function cmb2_metabox_form( $meta_box, $object_id = 0, $args = array() ) {
 		cmb2_print_metabox_form( $meta_box, $object_id, $args );
 	} else {
 		return cmb2_get_metabox_form( $meta_box, $object_id, $args );
+	}
+}
+
+if ( ! function_exists( 'date_create_from_format' ) ) {
+
+	/**
+	 * Reimplementation of DateTime::createFromFormat for PHP < 5.3. :(
+	 * Borrowed from http://stackoverflow.com/questions/5399075/php-datetimecreatefromformat-in-5-2
+	 *
+	 * @param $date_format
+	 * @param $date_value
+	 *
+	 * @return DateTime
+	 */
+	function date_create_from_format( $date_format, $date_value ) {
+
+		$schedule_format = str_replace(
+			array( 'M', 'Y', 'm', 'd', 'H', 'i', 'a' ),
+			array('%b', '%Y', '%m', '%d', '%H', '%M', '%p' ),
+			$date_format
+		);
+
+		/*
+		 * %Y, %m and %d correspond to date()'s Y m and d.
+		 * %I corresponds to H, %M to i and %p to a
+		 */
+		$parsed_time = strptime( $date_value, $schedule_format );
+
+		$ymd = sprintf(
+			/*
+			 * This is a format string that takes six total decimal
+			 * arguments, then left-pads them with zeros to either
+			 * 4 or 2 characters, as needed
+			 */
+			'%04d-%02d-%02d %02d:%02d:%02d',
+			$parsed_time['tm_year'] + 1900,  // This will be "111", so we need to add 1900.
+			$parsed_time['tm_mon'] + 1,      // This will be the month minus one, so we add one.
+			$parsed_time['tm_mday'],
+			$parsed_time['tm_hour'],
+			$parsed_time['tm_min'],
+			$parsed_time['tm_sec']
+		);
+
+		return new DateTime($ymd);
 	}
 }
