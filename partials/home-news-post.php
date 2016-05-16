b501f4a98adf7f8147b3b3e52614e0db2897e928
Patrick Best <patrickbest@patrickbest.com>
2016-05-16 17:27:31 -0500

    capitalize es date on home news posts

--- partials/home-news-post.php
+++ partials/home-news-post.php
@@ -1,7 +1,7 @@
 <div class="home-news-post">
   <a href="<?php the_permalink() ?>">
     <header class="home-news-post-header u-align-center">
-      <h4 class="home-news-post-date"><?php the_time('M., y') ?></h4>
+      <h4 class="home-news-post-date font-capitalize"><?php the_time('M., y') ?></h4>
       <h2 class="js-fix-widows"><?php the_title(); ?></h2>
     </header>
     <?php the_post_thumbnail('col-12'); ?>
